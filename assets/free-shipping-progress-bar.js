6eea9111ac3e6b789b2b03c5e851ad0bbe93e2cc
Sean Mitchell Tolentino <seanmi09353328910@gmail.com>
2025-11-27 17:54:04 +0800

    exclude gift cards in calculating the cart total and create a component for free-shipping-progress-bar

---
+++ assets/free-shipping-progress-bar.js
@@ -0,0 +1,12 @@
+class FreeShippingProgressBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    // Component is connected to the DOM
+  }
+}
+
+customElements.define('free-shipping-progress-bar', FreeShippingProgressBar);
+
